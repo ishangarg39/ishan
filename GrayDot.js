8f1bc851777e69ef859db11f6ef784b4b10622c4
ishangarg39 <83765411+ishangarg39@users.noreply.github.com>
2023-06-11 19:37:39 +0530

    first commit

---
+++ GrayDot.js
@@ -0,0 +1,95 @@
+const draggables = document.querySelectorAll('.draggable');
+const containers = document.querySelectorAll('.container');
+const button = document.querySelector('.button');
+
+// Store the original state of containers
+let originalState1 = [];
+let originalState2 = [];
+
+const firstContainer = document.querySelector('.container:first-child');
+firstContainer.childNodes.forEach(node => {
+    if (node.classList && node.classList.contains('draggable')) {
+        originalState1.push(node.cloneNode(true));
+    }
+});
+
+const secondContainer = document.querySelector('.container:last-child');
+secondContainer.childNodes.forEach(node => {
+    if (node.classList && node.classList.contains('draggable')) {
+        originalState2.push(node.cloneNode(true));
+    }
+});
+
+button.addEventListener('click', () => {
+    console.log('Reset button clicked');
+
+    // Clear the contents of all containers
+    containers.forEach(container => {
+        container.innerHTML = '';
+    });
+
+    // Restore the original state of the first container
+    originalState1.forEach(item => {
+        firstContainer.appendChild(item.cloneNode(true));
+    });
+
+    // Restore the original state of the second container
+    originalState2.forEach(item => {
+        secondContainer.appendChild(item.cloneNode(true));
+    });
+
+    // Reinitialize dragging functionality after a short delay
+    setTimeout(() => {
+        initializeDragging();
+    }, 10);
+});
+
+// Function to initialize dragging functionality
+function initializeDragging() {
+    // Attach dragstart and dragend event listeners to draggable elements
+    draggables.forEach(draggable => {
+        draggable.addEventListener('dragstart', () => {
+            draggable.classList.add('dragging');
+        });
+
+        draggable.addEventListener('dragend', () => {
+          alert("success")
+            draggable.classList.remove('dragging');
+        });
+    });
+
+    // Attach dragover event listener to containers
+    containers.forEach(container => {
+        container.addEventListener('dragover', e => {
+            e.preventDefault();
+            const afterElement = getDragAfterElement(container, e.clientY);
+            console.log(afterElement)
+            const draggable = document.querySelector('.dragging');
+            console.log(draggable)
+            if (draggable !== null) {
+                if (afterElement === null) {
+                    container.appendChild(draggable);
+                } else {
+                    container.insertBefore(draggable, afterElement);
+                }
+            }
+        });
+    });
+}
+
+// Initialize dragging functionality
+initializeDragging();
+
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')];
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child };
+        } else {
+            return closest;
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element;
+}
